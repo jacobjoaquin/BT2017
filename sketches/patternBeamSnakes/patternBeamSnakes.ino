ceac39a01f14b4453bc519d83fd4fd4c955555ae
Jacob Joaquin <jacobjoaquin@gmail.com>
2017-02-18 06:54:58 -0800

    Updated heads of the ino files

--- sketches/patternBeamSnakes/patternBeamSnakes.ino
+++ sketches/patternBeamSnakes/patternBeamSnakes.ino
@@ -1,35 +1,28 @@
 /*
-  patternBeamSnakes.ino
-  Jacob Joquin
+patternBeamSnakes.ino - Beam Snakes Pattern
+Coded by Jacob Joaquin.
 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-  Original OctoWS2811 Copyright Notice:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-  OctoWS2811 BasicTest.ino - Basic RGB LED Test
-  http://www.pjrc.com/teensy/td_libs_OctoWS2811.html
-  Copyright (c) 2013 Paul Stoffregen, PJRC.COM, LLC
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-
-  pin 2:  LED Strip #0
-  pin 14: LED strip #1
-  pin 7:  LED strip #2
+pin 2:  LED Strip #0
+pin 14: LED strip #1
+pin 7:  LED strip #2
 */
 
 #include <OctoWS2811.h>
