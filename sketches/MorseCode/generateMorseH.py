9e44895a4a88afa3b5071536b2bbc0f46361069c
Jacob Joaquin <jacobjoaquin@gmail.com>
2017-02-18 08:13:37 -0800

    Initial Morse pattern

---
+++ sketches/MorseCode/generateMorseH.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+quote = '''
+HELLO WORLD DISORIENT PRESENTS BEDOUIN TECH 2017 CREATE PARTICIPATE LOVE DISORIENT LOVES YOU
+'''
+
+dur = {'.': 1, '-': 3, 'letter': 3, 'space': 7, 'none': 0}
+morse = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---',
+    '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.'}
+
+data_table = {
+'.': '1',
+'-': '2',
+' ': '0'
+}
+
+encoded = ''
+
+# Process each character in quote
+for c in quote:
+    # Letter
+    if c in morse:
+        for m in morse[c]:
+            encoded += m;
+
+        encoded += dur['letter'] * ' ';
+
+    # Space
+    elif c == ' ':
+        encoded += (dur['space']  -  3) * ' '
+
+encoded += (dur['space']  -  3) * ' '
+
+
+print("'" + encoded + "'")
+print(len(encoded))
+
+output = '// File Generated with generateMorseH.py\n'
+output += '// ' + quote + '\n'
+output += 'const uint8_t faceTable[] PROGMEM = {'
+for c in encoded:
+    if c == ' ':
+        output += data_table[c] + ", "
+    elif  c == '.':
+        output += data_table[c] + ", "
+        output += '0' + ", "
+    elif c == '-':
+        output += data_table[c] + ", "
+        output += data_table[c] + ", "
+        output += data_table[c] + ", "
+        output += '0' + ", "
+
+output = output[:-2]
+output += '};\n'
+
+with open('encoded.h', 'w') as f:
+    f.write(output)
+
+print(output)
+print(encoded)
